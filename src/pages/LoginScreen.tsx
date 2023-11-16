a71b9066f7b6c701e384874b5c0284c820b04ee8
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-16 16:39:29 +0300

    page added

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-const LoginScreen = () => {
+const LoginScreen: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const loginButtonOnPress = () => {
+    alert("ssss");
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.page}>
+      <Pressable style={styles.button} onPress={loginButtonOnPress}>
+        <Text>Uygulamaya Giriş yap</Text>
+      </Pressable>
     </View>
   );
 };
 
 export { LoginScreen };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    borderRadius: 4,
+    backgroundColor: "lightblue",
+  },
+});
--- src/pages/SettingsScreen.tsx
+++ src/pages/SettingsScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const SettingsScreen = () => {
+const SettingsScreen: React.FC = () => {
   return (
     <View>
       <Text>SettingsScreen</Text>
