77f45e7b23384b625a2c5449350bff996ea213d2
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-16 15:29:18 +0300

    statusbar added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./src/pages";
+import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    // <StatusBar style="auto" />
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
