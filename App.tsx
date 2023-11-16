7ba0ecb3897f376039f03101cc3051a861b41f1a
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-16 16:29:18 +0300

    react navigation complated

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "./src/pages";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./src/navigation/AuthNavigator";
 
-const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const HomeScreen = () => {
+const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
