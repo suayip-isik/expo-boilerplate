9c55b8464b44350c62afb6be73417501e3cb1a57
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-18 19:22:20 +0300

    camera tests added

--- src/pages/CameraScreen.tsx
+++ src/pages/CameraScreen.tsx
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Camera, CameraType } from "expo-camera";
-import { COLOR_RED, COLOR_WHITE } from "../theme";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { MyCamera } from "../components";
 
-const CameraScreen = () => {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-
-  useEffect(() => {
-    requestPermission();
-    console.log(permission);
-  }, []);
-
-  const toggleCameraType = () => {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back
-    );
-  };
-
-  if (!permission?.granted) {
-    return <Text>Kamera ya izin verilmemiş </Text>;
-  }
-
+const CameraScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={toggleCameraType}>
-            <Text style={styles.buttonText}>Change Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+      <MyCamera />
     </View>
   );
 };
@@ -40,25 +15,5 @@ export { CameraScreen };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    position: "absolute",
-    bottom: 20,
-    alignSelf: "center",
-    borderWidth: 3,
-    borderColor: COLOR_WHITE,
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: COLOR_RED,
-  },
-  buttonText: {
-    color: COLOR_WHITE,
-    fontSize: 20
   },
 });
