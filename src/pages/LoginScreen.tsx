5d3604ab5c07a24e8580187812ea1e73bb52c177
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-18 09:35:19 +0300

    context added in LoginScrenn

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -2,17 +2,19 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useAuthContext } from "../context/auth/useAuthContext";
 
 const LoginScreen: React.FC = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const { login } = useAuthContext();
   const loginButtonOnPress = () => {
-    alert("ssss");
+    // login({ userName: "", password: "", isRememberMe: true });
   };
 
   return (
     <View style={styles.page}>
       <Pressable style={styles.button} onPress={loginButtonOnPress}>
-        <Text>Uygulamaya Giriş yap {process.env.EXPO_PUBLIC_API_URL?.toString()}</Text>
+        <Text>Login Screen</Text>
       </Pressable>
     </View>
   );
