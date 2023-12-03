4e778fabd6632956e95c66fb5f7b6e11aa16783b
Şuayip Işık <suayip.isik.16@gmail.com>
2023-12-03 22:48:10 +0300

    path alias added and fix,

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".ios.jsx",
+            ".android.jsx",
+            ".js",
+            ".jsx",
+            ".json",
+            ".ts",
+            ".tsx",
+          ],
+          root: ["."],
+          alias: {
+            "@components": "./src/components",
+            "@navigation": "./src/navigation",
+            "@context": "./src/context",
+          },
+        },
+      ],
+    ],
   };
 };
--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { useAuthContext } from "./context/auth/useAuthContext";
-import AuthNavigator from "./navigation/AuthNavigator";
-import MainTabNavigator from "./navigation/MainTabNavigator";
+import { useAuthContext } from "@context/auth";
+import AuthNavigator from "@navigation/AuthNavigator";
+import MainTabNavigator from "@navigation/MainTabNavigator";
 
 const Router = () => {
   // const { isAuthenticated } = useAuthContext();
